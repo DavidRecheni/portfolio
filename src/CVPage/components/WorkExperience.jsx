0a153b8e36241bfc24660e19835fbf58780dc694
Deivid <david_recheni@hotmail.com>
2021-12-28 20:37:52 +0100

    Updated work exp

--- src/CVPage/components/WorkExperience.jsx
+++ src/CVPage/components/WorkExperience.jsx
@@ -9,7 +9,7 @@ const experience = [
     title: 'Lead Front-end Developer',
     stack: 'React, Tailwind, Eslint, React-query, Storybooks',
     description:
-      '• Design and implement a new version of their MVP from scratch . • Hire a front-end team. • Perform code reviews and enforce high quality coding standards. • Validate UX Design decisions and propose solutions. '
+      '• Design and implement a new version of their MVP from scratch . • Delegate tasks and control progress • Perform code reviews and enforce high quality coding standards. • Built a front-end team. • Mentor Front-end devs • Validate UX Design decisions and propose solutions. '
     ,
   },
   {
@@ -21,12 +21,20 @@ const experience = [
     description: '• Develop micro-frontend tools for company suppliers (ReactJS) • Expand and maintain employee app (Angular). • Company Hardware maintainance. • Mentor dev intern. • Help ideate and create creative solutions to company problems. • UX / UI Designer',
   },
   {
-    date: { start: '06/2018', end: '03/2020' },
+    date: { start: '12/2018', end: '03/2020' },
     place: 'La Plata, ARG',
     institution: { name: 'Avanade', link: 'https://www.avanade.com/' },
     title: 'Lead Full-Stack Developer',
     stack: 'AngularJS, NodeJS, Bootstrap, .NET',
-    description: '• Develop web-apps in a very fast moving enviroment • Perform code reviews and enforce high quality coding standards. • Mentor new joiners  • Help the team implement SCRUM     ',
+    description: '• Develop web-apps in a very fast moving enviroment • Perform code reviews and enforce high quality coding standards. • Delegate tasks and control progress • Mentor new joiners  • Help the team implement SCRUM     ',
+  },
+  {
+    date: { start: '06/2018', end: '12/2018' },
+    place: 'La Plata, ARG',
+    institution: { name: 'Avanade', link: 'https://www.avanade.com/' },
+    title: 'Sr Full-Stack Developer',
+    stack: 'AngularJS, NodeJS, Bootstrap, .NET',
+    description: '• Develop web-apps in a very fast moving enviroment • Mentor new joiners',
   },
 ];
 
