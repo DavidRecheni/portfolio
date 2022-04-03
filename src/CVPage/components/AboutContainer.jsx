4005bc5124f3708490ed0b7e9e2924f764a66b41
Deivid <david_recheni@hotmail.com>
2022-04-03 12:16:58 +0200

    Updated aboutme

--- src/CVPage/components/AboutContainer.jsx
+++ src/CVPage/components/AboutContainer.jsx
@@ -6,9 +6,9 @@ export default function AboutContainer() {
       <Header>About me</Header>
       <div className="flex justify-start">
         <p className="max-w-prose text-justify text-sm sm:text-base">
-          Hallo! I’m a 29 y.o. Argentinian that lives in Berlin since February 2020.
+          Hallo! I’m a 30 y.o. Argentinian that lives in Berlin since February 2020.
           I’m always eager to learn new things. I feel comfortable accepting responsibilities and
-          coordinating teams.Due to my studies as Designer and Dev, and my long experience working
+          coordinating teams. Due to my studies as Designer and Dev, and my long experience working
           as freelancer, I can ideate, design, develop and deploy solutions from scratch.
           I’ve worked as Full-stack developer most of my carreer but I decided to specialize in
           Frontend. I’ve been working exclusively with React since I arrived to Germany
